60447eb72c669bbe0d9a0acc531b7865e644a921
Maverick2875 <145050797+Maverick2875@users.noreply.github.com>
2023-10-11 21:47:54 -0400

    Add files via upload

---
+++ src/Model/Patient.java
@@ -0,0 +1,132 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author USER
+ */
+public class Patient {
+    
+    private String firstName;
+    private String lastName;
+    private String age;
+    private String gender;
+    private String email;
+    private String patientType;
+    private String message;
+
+    private String FilePath;
+
+ 
+
+ 
+
+    
+    public String getFilePath(){
+        return FilePath;
+    }
+    public void setFilePath(String FilePath){
+        this.FilePath = FilePath;
+    }
+    /**
+     * @return the firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * @param firstName the firstName to set
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * @return the lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * @param lastName the lastName to set
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * @return the age
+     */
+    public String getAge() {
+        return age;
+    }
+
+    /**
+     * @param age the age to set
+     */
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    /**
+     * @return the gender
+     */
+    public String getGender() {
+        return gender;
+    }
+
+    /**
+     * @param gender the gender to set
+     */
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the patientType
+     */
+    public String getPatientType() {
+        return patientType;
+    }
+
+    /**
+     * @param patientType the patientType to set
+     */
+    public void setPatientType(String patientType) {
+        this.patientType = patientType;
+    }
+
+    /**
+     * @return the message
+     */
+    public String getMessage() {
+        return message;
+    }
+
+    /**
+     * @param message the message to set
+     */
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    
+}
